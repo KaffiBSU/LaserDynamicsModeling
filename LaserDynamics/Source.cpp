bc3c1f5a015332ec19a8ab3eae71dbba9454e6be
un-drone <0v3r9r1v3@gmail.com>
2017-03-20 00:10:08 +0300

    Run, wave, run
    Волна бежит. Пока что простейшим методом и без констант, но бежит.

--- LaserDynamics/Source.cpp
+++ LaserDynamics/Source.cpp
@@ -8,14 +8,18 @@
 #include <string>
 #include <iterator>
 
-const size_t xpoints = 10; // число разбиений по пространству
-const float tmax = 10E-4f;
-const float dt = 50E-5f; // можно вычислять по соотношению из численной схемы
+const size_t xpoints = 200; // число разбиений по пространству
+const double tmax = 10E-4f;
+const double dt = 10E-5f; // можно вычислять по соотношению из численной схемы
 const int tpoints = tmax / dt; // потери данных; для шага по времени, возможно, некритично
 
-void SetToZero(float *u)
+void SetInitialCondition(double *u)
 {
-	for (int i = 0; i < xpoints; i++) u[i] = 0;
+	for (int i = 0; i < xpoints; i++)
+	{
+		if (i < 1 || i > 2) u[i] = 0;
+		else u[i] = 1;
+	}
 }
 
 /* проверка ошибок */
@@ -56,30 +60,30 @@ int main(void){
 	checkErr(devices.size() > 0 ? CL_SUCCESS : -1, "devices.size() > 0");
 
 	/* создание буферов */
-	float * inputPlus = new float[xpoints]; // double?
-	SetToZero(inputPlus);
+	double * inputPlus = new double[xpoints];
+	SetInitialCondition(inputPlus);
 
 	std::cout << "inputPlus: " << std::endl;
 	for (int i = 0; i < xpoints; i++) std::cout << i << " " << inputPlus[i] << std::endl;
 
-	cl::Buffer inputPlusBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(float), inputPlus, &err);
+	cl::Buffer inputPlusBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(double), inputPlus, &err);
 	checkErr(err, "Buffer::Buffer()");
 
-	float * inputMinus = new float[xpoints]; // double?
-	SetToZero(inputMinus);
+	double * inputMinus = new double[xpoints];
+	SetInitialCondition(inputMinus);
 
 	std::cout << "inputMinus: " << std::endl;
 	for (int i = 0; i < xpoints; i++) std::cout << i << " " << inputMinus[i] << std::endl;
 
-	cl::Buffer inputMinusBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(float), inputMinus, &err);
+	cl::Buffer inputMinusBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(double), inputMinus, &err);
 	checkErr(err, "Buffer::Buffer()");
 
-	float * outputPlus = new float[xpoints]; // double?
-	cl::Buffer outputPlusBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(float), outputPlus, &err);
+	double * outputPlus = new double[xpoints];
+	cl::Buffer outputPlusBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(double), outputPlus, &err);
 	checkErr(err, "Buffer::Buffer()");
 
-	float * outputMinus = new float[xpoints]; // double?
-	cl::Buffer outputMinusBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(float), outputMinus, &err);
+	double * outputMinus = new double[xpoints];
+	cl::Buffer outputMinusBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, xpoints * sizeof(double), outputMinus, &err);
 	checkErr(err, "Buffer::Buffer()");
 
 	/* создание объекта программы */
@@ -103,50 +107,70 @@ int main(void){
 	checkErr(err, "Kernel::setArg()");
 	err = kernel.setArg(3, outputMinusBuffer);
 	checkErr(err, "Kernel::setArg()");
-	err = kernel.setArg(4, (int)xpoints); // костыль с (int), но вроде нормально
-	checkErr(err, "Kernel::setArg()");
 
 	/* создание очереди */
 	cl::CommandQueue queue(context, devices[0], 0, &err);
 	checkErr(err, "CommandQueue::CommandQueue()");
 	cl::Event event;
 
-	/* вычисления */
-	for (int i = 0; i < tpoints; i++)
+	FILE* gnuplotPipe = _popen("D:/gnuplot/bin/gnuplot.exe", "w");
+	fprintf(gnuplotPipe, "set grid\n");
+	if (gnuplotPipe)
 	{
-		err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(xpoints), cl::NullRange, NULL, &event);
-		checkErr(err, "ComamndQueue::enqueueNDRangeKernel()");
+		fprintf(gnuplotPipe, "set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb\"white\" behind\n"); // заливка цветом(белым)
+		fprintf(gnuplotPipe, "set xrange [0:200]\n"); // интервал по x
+		fprintf(gnuplotPipe, "set yrange [0:1]\n"); // интервал по y
 
-		/* чтение буферов */
-		event.wait();
-		err = queue.enqueueReadBuffer(outputPlusBuffer, CL_TRUE, 0, xpoints * sizeof(float), outputPlus);
-		checkErr(err, "CommandQueue::enqueueReadBuffer()");
-		err = queue.enqueueReadBuffer(outputMinusBuffer, CL_TRUE, 0, xpoints * sizeof(float), outputMinus);
-		checkErr(err, "CommandQueue::enqueueReadBuffer()");
-		
-		std::cout << "Result; iteration #" << i << std::endl;
-		std::cout << "Plus:" << std::endl;
-		for (int j = 0; j < xpoints; j++)
+		fprintf(gnuplotPipe, "plot [][0:1] 2\n"); //отображение окна
+		printf("Press enter when window will appear");
+		fflush(gnuplotPipe);  //очистка
+
+		/* вычисления */
+		for (int i = 0; i < 1000/*tpoints*/; i++)
 		{
+			err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(4), cl::NDRange(4), NULL, &event);
+			checkErr(err, "ComamndQueue::enqueueNDRangeKernel()");
+
+			/* чтение буферов */
+			event.wait();
+			err = queue.enqueueReadBuffer(outputPlusBuffer, CL_TRUE, 0, xpoints * sizeof(double), outputPlus);
+			checkErr(err, "CommandQueue::enqueueReadBuffer()");
+			err = queue.enqueueReadBuffer(outputMinusBuffer, CL_TRUE, 0, xpoints * sizeof(double), outputMinus);
+			checkErr(err, "CommandQueue::enqueueReadBuffer()");
+			fprintf(gnuplotPipe, "plot '-' using 1:2 with points lc rgb 'black' pt 7\n");
+			for (int i = 0; i < xpoints; i++)
+			{
+				fprintf(gnuplotPipe, "%d, %lf\n", i, outputPlus[i]);
+			}
+			fprintf(gnuplotPipe, "e\n");
+			fflush(gnuplotPipe);
+			/*std::cout << "Result; iteration #" << i << std::endl;
+			std::cout << "Plus:" << std::endl;
+			for (int j = 0; j < xpoints; j++)
+			{
 			std::cout << outputPlus[j] << " ";
-		}
-		std::cout << std::endl << "Minus " << std::endl;
-		for (int j = 0; j < xpoints; j++)
-		{
+			}*/
+			/*std::cout << std::endl << "Minus " << std::endl;
+			for (int j = 0; j < xpoints; j++)
+			{
 			std::cout << outputMinus[j] << " ";
+			}*/
+			std::cout << std::endl;
+
+			/* переброс указателей */
+			std::swap(inputPlus, outputPlus);
+			std::swap(inputMinus, outputMinus);
+
+			/* перезапись входных буферов */
+			err = queue.enqueueWriteBuffer(inputPlusBuffer, CL_TRUE, 0, xpoints * sizeof(double), inputPlus);
+			checkErr(err, "CommandQueue::enqueueWriteBuffer()");
+			err = queue.enqueueWriteBuffer(inputMinusBuffer, CL_TRUE, 0, xpoints * sizeof(double), inputMinus);
+			checkErr(err, "CommandQueue::enqueueWriteBuffer()");
 		}
-		std::cout << std::endl;
+		printf("press enter to continue...");
+		getchar();
 
-		/* переброс указателей */
-		std::swap(inputPlus, outputPlus);
-		std::swap(inputMinus, outputMinus);
-
-		/* перезапись входных буферов */
-		err = queue.enqueueWriteBuffer(inputPlusBuffer, CL_TRUE, 0, xpoints * sizeof(float), inputPlus);
-		checkErr(err, "CommandQueue::enqueueWriteBuffer()");
-		err = queue.enqueueWriteBuffer(inputMinusBuffer, CL_TRUE, 0, xpoints * sizeof(float), inputMinus);
-		checkErr(err, "CommandQueue::enqueueWriteBuffer()");
+		fprintf(gnuplotPipe, "exit\n");
 	}
-
 	return EXIT_SUCCESS;
 }
--- LaserDynamics/kernel.cl
+++ LaserDynamics/kernel.cl
@@ -1,11 +1,104 @@
-﻿__kernel void solver(__global float * inputPlus, __global float * outputPlus,
-					 __global float * inputMinus, __global float * outputMinus, int xpoints)
-{
+﻿#define xpoints 200
+#define local_xpoints 50
+#define size 4
 
-	for (int i = 0; i < xpoints; i++)
+__constant double A = 1;
+__constant double length_res = 1; // длина резонатора
+__constant double dt = 0.0005;
+__constant double dx = 0.01;
+
+/* граничные условия, зависящие от времени */
+double x0()
+{
+	return 0;
+}
+
+double xmax()
+{
+	return 0;
+}
+
+void next_step(double* in_plus, double* in_minus, double* out_plus, double* out_minus,
+			   __local double* lefts_plus, __local double* rights_plus,	__local double* lefts_minus, 
+			   __local double* rights_minus, int my_id)
+{
+	/* вычисление внешних граничных точек */
+	if (my_id == 0)
 	{
-		outputPlus[i] = inputPlus[i] + 2;
-		outputMinus[i] = inputMinus[i] + 3;
+		out_plus[0] = x0();
+		out_minus[0] = x0();
+	}
+	if (my_id == (size - 1))
+	{
+		out_plus[local_xpoints - 1] = xmax();
+		out_minus[local_xpoints - 1] = xmax();
+	}
+	/* вычисление внутренних граничных точек */
+	//левой:
+	if (my_id != 0)
+	{
+		out_plus[0] = in_plus[0] - dt / dx * (in_plus[0] - rights_plus[my_id - 1]);
+		out_minus[0] = in_minus[0] - dt / dx * (in_minus[0] - rights_minus[my_id - 1]);
+	}
+	//правой:
+	/*if (my_id != (size - 1))
+	{
+		out_plus[local_xpoints - 1] = in_plus[local_xpoints - 1] - dt / dx * (in_plus[local_xpoints - 1] - lefts_plus[my_id + 1]);
+		out_minus[local_xpoints - 1] = in_minus[local_xpoints - 1] - dt / dx * (in_minus[local_xpoints - 1] - lefts_minus[my_id + 1]);
+	}*/
+	/* вычисление внутренних точек */
+	for (int i = 1; i < local_xpoints/* - 1*/; i++)
+	{
+		out_plus[i] = in_plus[i] - dt / dx * (in_plus[i] - in_plus[i - 1]);
+		out_minus[i] = in_minus[i] - dt / dx * (in_minus[i] - in_minus[i - 1]);
+	}
+}
+
+__kernel void solver(__global double * inputPlus, __global double * outputPlus,
+					 __global double * inputMinus, __global double * outputMinus)
+{
+	int my_id = get_global_id(0);
+
+	/* вычисления будут проводиться в приватной (быстрой) памяти */
+	__private double in_plus[local_xpoints];
+	__private double in_minus[local_xpoints];
+	__private double out_plus[local_xpoints];
+	__private double out_minus[local_xpoints];
+
+	/* общая память */
+	__local double lefts_plus[size - 1];
+	__local double rights_plus[size - 1];
+	__local double lefts_minus[size - 1];
+	__local double rights_minus[size - 1];
+
+	/* worker'ы разбирают массивы */
+	for (int i = 0; i < local_xpoints; i++)
+	{
+		in_plus[i] = inputPlus[my_id * local_xpoints + i];
+		in_minus[i] = inputMinus[my_id * local_xpoints + i];
 	}
 
+	/* складываем левые и правые точки в локальную память */
+	/* для этой численной схемы lefts не нужны */
+	/*if (my_id != 0)
+	{
+		lefts_plus[my_id - 1] = in_plus[0];
+		lefts_minus[my_id - 1] = in_minus[0];
+	}*/
+	if (my_id != (size - 1))
+	{
+		rights_plus[my_id] = in_plus[local_xpoints - 1];
+		rights_minus[my_id] = in_minus[local_xpoints - 1]; 
+	}
+
+	/* вычисления *////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	next_step(in_plus, in_minus, out_plus, out_minus,
+				lefts_plus, rights_plus, lefts_minus, rights_minus, my_id);
+	/* конец вычислений *//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	for (int i = 0; i < local_xpoints; i++)
+	{
+		outputPlus[my_id * local_xpoints + i] = out_plus[i];
+		outputMinus[my_id * local_xpoints + i] = out_minus[i];
+	}
 }
